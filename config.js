4c123a7be17efd9fa049ff1c4c27e57d62a1231e
André Samuelsson <andresamuelsson94@gmail.com>
2016-04-08 01:50:00 +0200

    Add admin field to config-

--- config.js
+++ config.js
@@ -14,5 +14,7 @@ module.exports = {
     },
     whitelist: [],
     //Blacklist is only used if the whitelist is empty
-    blacklist: []
+    blacklist: [],
+    //Is allowed to edit black/whitelist on the fly
+    admins: []
 }
