40e18e0935cb095acf506b980a1bd18edafb50d6
André Samuelsson <andresamuelsson94@gmail.com>
2016-04-11 19:44:24 +0200

    Make test names more generic

--- test/specs.js
+++ test/specs.js
@@ -3,51 +3,51 @@ const expect = require('expect');
 const index = require('../index');
 
 describe('isAllowedToAskForIssue', () => {
-    it("should allow Tejp when he's both in black- and whitelist", () => {
-      const whitelist = ['Tejp']
-      const blacklist = ['Tejp']
+    it("should allow when in both black- and whitelist", () => {
+      const whitelist = ['Foo']
+      const blacklist = ['Foo']
       expect(
-        index.isAllowedToAskForIssue('Tejp', whitelist, blacklist)
+        index.isAllowedToAskForIssue('Foo', whitelist, blacklist)
       ).toEqual(true)
     });
 
-    it("should allow Tejp when he's in whitelist", () => {
-      const whitelist = ['Tejp']
+    it("should allow when in whitelist", () => {
+      const whitelist = ['Foo']
       const blacklist = []
       expect(
-        index.isAllowedToAskForIssue('Tejp', whitelist, blacklist)
+        index.isAllowedToAskForIssue('Foo', whitelist, blacklist)
       ).toEqual(true)
     });
 
-    it("should NOT allow Tejp when he's in blacklist", () => {
+    it("should NOT allow when in blacklist and there is no whitelist", () => {
       const whitelist = []
-      const blacklist = ['Tejp']
+      const blacklist = ['Foo']
       expect(
-        index.isAllowedToAskForIssue('Tejp', whitelist, blacklist)
+        index.isAllowedToAskForIssue('Foo', whitelist, blacklist)
       ).toEqual(false)
     });
 
-    it("should allow Tejp when black- and whitelist both are empty", () => {
+    it("should allow when black- and whitelist both are empty", () => {
       const whitelist = []
       const blacklist = []
       expect(
-        index.isAllowedToAskForIssue('Tejp', whitelist, blacklist)
+        index.isAllowedToAskForIssue('Foo', whitelist, blacklist)
       ).toEqual(true)
     });
 
-    it("should allow Tejp when blacklist exists but he's not in it", () => {
+    it("should allow when blacklist exists but not in it", () => {
       const whitelist = []
       const blacklist = ['SomeOtherGuy']
       expect(
-        index.isAllowedToAskForIssue('Tejp', whitelist, blacklist)
+        index.isAllowedToAskForIssue('Foo', whitelist, blacklist)
       ).toEqual(true)
     });
 
-    it("should NOT allow Tejp when whitelist exists but he's not in it", () => {
+    it("should NOT allow when whitelist exists but he's not in it", () => {
       const whitelist = ['SomeOtherGuy']
       const blacklist = []
       expect(
-        index.isAllowedToAskForIssue('Tejp', whitelist, blacklist)
+        index.isAllowedToAskForIssue('Foo', whitelist, blacklist)
       ).toEqual(false)
     });
 });
